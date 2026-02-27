55f932857ca8f1eb4a4893af930cb190bc39502e
Lewis Wang <i@lewis.si>
2026-02-27 16:30:11 +0800

    Modify color values in build script
    Updated color values in XML and smali files for the APK.

--- build.sh
+++ build.sh
@@ -13,19 +13,16 @@ rm -rf patched patched_signed.apk
 ./apktool d latest.apk -o patched
 rm -rf patched/META-INF
 
-sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">#ff000000<\/color>/g' patched/res/values-night/colors.xml
+sed -i 's/<color name="fx_mobile_surface">.*/<color name="fx_mobile_surface">#ff000000<\/color>/g' patched/res/values-night/colors.xml
+sed -i 's/<color name="fx_mobile_background">.*/<color name="fx_mobile_background">#ff000000<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
-sed -i 's/<color name="fx_mobile_action_color_secondary">.*/<color name="fx_mobile_action_color_secondary">#ff25242b<\/color>/g' patched/res/values-night/colors.xml
-sed -i 's/<color name="button_material_dark">.*/<color name="button_material_dark">#ff25242b<\/color>/g' patched/res/values/colors.xml
-sed -i 's/ff1c1b22/ff15141a/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff2b2a33/ff000000/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff42414d/ff15141a/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff52525e/ff15141a/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/1c1b22/000000/g' patched/assets/extensions/readerview/readerview.css
-sed -i 's/eeeeee/e3e3e3/g' patched/assets/extensions/readerview/readerview.css
-sed -i 's/mipmap\/ic_launcher_round/drawable\/ic_launcher_foreground/g' patched/res/drawable-v23/splash_screen.xml
-sed -i 's/160\.0dip/200\.0dip/g' patched/res/drawable-v23/splash_screen.xml
 
+# Smali patching
+sed -i 's/ff2b2a33/ff000000/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff42414d/ff15141a/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff52525e/ff15141a/g' patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+
+# Recompile the APK
 ./apktool b patched -o patched.apk
 
 zipalign 4 patched.apk patched_signed.apk
